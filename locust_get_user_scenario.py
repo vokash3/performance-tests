05aaddab972e45bfe89838846957e56548cb580a
Vladimir Kashin <vokash3@yandex.ru>
2025-12-13 22:13:27 +0300

    10.3 Применение HTTP API клиентов в нагрузочных сценариях

--- locust_get_user_scenario.py
+++ locust_get_user_scenario.py
@@ -1,43 +1,49 @@
 # python -m locust -f locust_get_user_scenario.py --class-picker --processes 2 --csv report.csv --csv-full-history --json --json-file report --html report.html -u 300 -r 10 -t 1m
 
-from locust import HttpUser, between, task, constant_pacing
+from locust import HttpUser, between, task, constant_pacing, User
 
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 from tools.fakers import fake  # генератор случайных данных
 
 
-class GetUserScenarioUser(HttpUser):
+class GetUserScenarioUser(User):
+    """
+    Наследуемся от User вместо HttpUser/FastHttpUser,
+    чтобы не создавать экземпляр встроенного API клиента (Requests/geventhttpclient)
+    и пользоваться только возможностями гринлетов Locust.
+    """
     # Пауза между запросами для каждого виртуального пользователя (в секундах)
     # wait_time = between(1, 3)
     wait_time = constant_pacing(1)
+    # Будет проигнорировано под капотом, так как уже зафиксировано в кастомном клиенте,
+    # но оставляем, чтобы в UI не смущал указанный целевой хост
     host = "http://155.212.171.137:8003"
 
-    # В этой переменной будем хранить данные созданного пользователя
-    user_data: dict
+    # Поле, в котором будет храниться экземпляр нашего API клиента
+    users_gateway_client: UsersGatewayHTTPClient
+    # Поле, куда мы сохраним ответ после создания пользователя
+    create_user_response: CreateUserResponseSchema
 
     def on_start(self) -> None:
         """
         Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
         Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
         """
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
+        # Шаг 1: создаем API клиент, встроенный в экосистему Locust (с хуками и поддержкой сбора метрик)
+        # Передаём self.environment – объект окружения Locust с данными о запросах,
+        # которые используются в статистике генератора
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
 
-        # Сохраняем полученные данные, включая ID пользователя
-        self.user_data = response.json()
+        # Шаг 2: создаем пользователя через API
+        # Помним, что пайдентик сам создаст нужную модель с помощью default_factory
+        self.create_user_response = self.users_gateway_client.create_user()
 
     @task
     def get_user(self):
         """
         Основная нагрузочная задача: получение информации о пользователе.
-        Здесь мы выполняем GET-запрос к /api/v1/users/{user_id}.
+        Здесь мы выполняем GET-запрос к /api/v1/users/{user_id},
+        используя кастомный API клиент на базе httpx.
         """
-        self.client.get(
-            f"/api/v1/users/{self.user_data['user']['id']}",
-            name="/api/v1/users/{user_id}"  # Явное указание имени группы запросов
-        )
+        self.users_gateway_client.get_user(self.create_user_response.user.id)
